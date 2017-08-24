0acaf55f6aa0af4bbdbc3d6dc9bace1520058994
trupovar13 <trupovar13@gmail.com>
2017-08-24 08:49:38 +0300

    Udate add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 function something()
 {
 
-		var x = window.localStorage.getItem('bbb');
+		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
 		x = x * 1 + 1;
 
-		window.localStorage.setItem('bbb', x);
+		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 		alert(x);
 	
@@ -13,5 +13,9 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id);	
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
